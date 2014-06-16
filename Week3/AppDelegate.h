576150106184b8306d33865e0e59870914d0b02c
Fernando Garcia Corrochano <ferBook@FerBook.local>
2014-06-16 10:21:27 +0200

    Initial Commit

---
+++ Week3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week3
+//
+//  Created by Fernando Garcia Corrochano on 16/06/14.
+//  Copyright (c) 2014 ironHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Week3
+//
+//  Created by Fernando Garcia Corrochano on 16/06/14.
+//  Copyright (c) 2014 ironHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
